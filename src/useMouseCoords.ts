fa7596925c934a2bab374d023e1f95c7bd1858c9
Sam Haakman <sam@josephmark.studio>
2022-09-16 10:47:11 +1000

    render-stable functions for useMouseCoords

--- src/useMouseCoords.ts
+++ src/useMouseCoords.ts
@@ -1,33 +1,38 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, useRef, useCallback } from "react"
 
 /**
  * Fetch current mouse coordinates
  *
- * @returns [number, number]
+ * @returns [x: number, y: number]
  */
 export default function useMouseCoords() {
   const [coords, setCoords] = useState([0, 0])
+  const awaitingIdle = useRef(false)
+
+  const mouseFollower = useCallback((e: MouseEvent) => {
+    awaitingIdle.current = false
+    setCoords([e.clientX, e.clientY])
+  }, [])
+
+  const mouseFollowerCaller = useCallback(
+    (e: MouseEvent) => {
+      if (awaitingIdle.current) {
+        return
+      }
+      requestAnimationFrame(() => mouseFollower(e))
+    },
+    [mouseFollower]
+  )
+
   useEffect(() => {
     if (typeof window === "undefined") {
       return () => {}
     }
-    let awaitingIdle = false
-    const mouseFollower = (e: MouseEvent) => {
-      awaitingIdle = false
-      setCoords([e.clientX, e.clientY])
-    }
-    const mouseFollowerCaller = (e: MouseEvent) => {
-      if (awaitingIdle) {
-        return
-      }
-      awaitingIdle = true
-      requestAnimationFrame(() => mouseFollower(e))
-    }
     document.addEventListener("mousemove", mouseFollowerCaller)
     return () => {
-      window.removeEventListener("mousemove", mouseFollowerCaller)
+      document.removeEventListener("mousemove", mouseFollowerCaller)
     }
-  }, [])
+  }, [mouseFollowerCaller])
 
   return coords
 }
